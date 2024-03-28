9bc14fe57aa8a974f71f7dc8a87d258cb5366bef
urosrelic <urosrelicc@gmail.com>
2024-03-28 13:58:06 +0100

    chore: remove label from fab options

--- src/utils/FabOptions.ts
+++ src/utils/FabOptions.ts
@@ -1,11 +1,9 @@
 export const fabOptions = [
   {
-    label: 'Option 1',
     iconPath: '/add.svg',
     onClick: () => console.log('Option 1 clicked'),
   },
   {
-    label: 'Option 2',
     iconPath: '/checklist.svg',
     onClick: () => console.log('Option 2 clicked'),
   },
