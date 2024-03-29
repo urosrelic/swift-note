767813fdc64fef2a37f676a4832e75f5be3538a0
urosrelic <urosrelicc@gmail.com>
2024-03-29 02:26:31 +0100

    feat: add home page to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Fab from './components/FAB/Fab';
 import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home/Home';
 
 function App() {
   const [gridView, setGridView] = useState<boolean>(true);
@@ -9,6 +10,7 @@ function App() {
     <>
       <div className='app'>
         <Navbar gridView={gridView} setGridView={setGridView} />
+        <Home gridView={gridView} />
         <Fab />
       </div>
     </>
