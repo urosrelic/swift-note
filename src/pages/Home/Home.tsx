b3ffc5a0d9befa657816327b1c5f1ead08a8d973
urosrelic <urosrelicc@gmail.com>
2024-03-29 02:25:05 +0100

    feat: update home page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import Notes from '../../components/Notes/Notes';
+import { GridProps } from '../../utils/GridProps';
+import './Home.css';
+const Home = ({ gridView }: GridProps) => {
+  return (
+    <div className='home'>
+      <Notes gridView={gridView} />
+    </div>
+  );
+};
+
+export default Home;
