ba6f849eb8fe54322b523a9599ed72f7ab097aa5
urosrelic <urosrelicc@gmail.com>
2024-03-29 02:35:00 +0100

    chore: remove unused css import

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,5 @@
 import Notes from '../../components/Notes/Notes';
 import { GridProps } from '../../utils/GridProps';
-import './Home.css';
 const Home = ({ gridView }: GridProps) => {
   return (
     <div className='home'>
