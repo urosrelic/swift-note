e4fa6d09668fb0fdf7dd80c46a84d17524bc217f
urosrelic <urosrelicc@gmail.com>
2024-04-01 14:35:01 +0200

    feat: add register modal
    Render login and register form based on condition

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,26 +4,48 @@ import Modal from '../../components/Modal/Modal';
 import Navbar from '../../components/Navbar/Navbar';
 import Notes from '../../components/Notes/Notes';
 import { ModalProvider } from '../../context/ModalContext';
+
 const Home = () => {
   const [gridView, setGridView] = useState<boolean>(true);
+  const [isRegisterModal, setIsRegisterModal] = useState<boolean>(false);
+
+  const loginModal = () => (
+    <Modal>
+      <span className='modal-title'>Login</span>
+      <input type='email' placeholder='Email' />
+      <input type='password' placeholder='Password' />
+      <button className='modal-btn'>Login</button>
+      <div className='modal-redirect'>
+        <p>Don't have an account?</p>
+        <p onClick={() => setIsRegisterModal(true)}>
+          Click <span>here</span> to register
+        </p>
+      </div>
+    </Modal>
+  );
+
+  const registerModal = () => (
+    <Modal>
+      <span className='modal-title'>Register</span>
+      <input type='email' placeholder='Email' />
+      <input type='password' placeholder='Password' />
+      <input type='password' placeholder='Confirm password' />
+      <button className='modal-btn'>Register</button>
+      <div className='modal-redirect'>
+        <p>Already have an account?</p>
+        <p onClick={() => setIsRegisterModal(false)}>
+          Click <span>here</span> to login
+        </p>
+      </div>
+    </Modal>
+  );
 
   return (
     <ModalProvider>
       <div className='home'>
         <Navbar gridView={gridView} setGridView={setGridView} />
         <Notes gridView={gridView} />
-        <Modal>
-          <span className='modal-title'>Login</span>
-          <input type='email' placeholder='Email' />
-          <input type='password' placeholder='Password' />
-          <button className='login-btn'>Login</button>
-          <div className='modal-redirect'>
-            <p>Don't have an account?</p>
-            <p>
-              Click <span>here</span> to register
-            </p>
-          </div>
-        </Modal>
+        {isRegisterModal ? registerModal() : loginModal()}
         <Fab />
       </div>
     </ModalProvider>
