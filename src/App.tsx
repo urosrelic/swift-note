9fe573ef66ca46eeebf8d5780ed0712bb6374274
urosrelic <urosrelicc@gmail.com>
2024-04-01 14:28:15 +0200

    feat: add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 
 function App() {
   return (
-    <>
-      <div className='app'>
-        <Home />
-      </div>
-    </>
+    <div className='app'>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
