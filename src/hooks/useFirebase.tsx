c1f9a59207bd989f1dcf63f950030718f5505c2c
urosrelic <urosrelicc@gmail.com>
2024-04-02 22:48:18 +0200

    feat: set loading and errors

--- src/hooks/useFirebase.tsx
+++ src/hooks/useFirebase.tsx
@@ -15,20 +15,31 @@ import { Note } from '../utils/types/Note';
 const useFirebase = (currentUser: FirebaseCurrentUser | null) => {
   const notesRef = useMemo(() => collection(db, 'notes'), []);
 
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>('');
+
   const [notes, setNotes] = useState<Note[] | null>(null);
 
   useEffect(() => {
     if (currentUser) {
+      setLoading(true);
       const q = query(notesRef, where('userId', '==', currentUser.uid));
-      const unsub = onSnapshot(q, (querySnapshot) => {
-        const items: Note[] = [];
-        querySnapshot.forEach((doc) => {
-          const data = doc.data() as Note;
-          items.push({ noteId: doc.id, ...data });
-        });
-        setNotes(items);
-        console.log(items);
-      });
+      const unsub = onSnapshot(
+        q,
+        (querySnapshot) => {
+          const items: Note[] = [];
+          querySnapshot.forEach((doc) => {
+            const data = doc.data() as Note;
+            items.push({ noteId: doc.id, ...data });
+          });
+          setNotes(items);
+          setLoading(false);
+        },
+        (error) => {
+          setError((error as Error).message || 'An error occurred');
+          setLoading(false);
+        }
+      );
       return () => {
         unsub();
       };
@@ -38,23 +49,29 @@ const useFirebase = (currentUser: FirebaseCurrentUser | null) => {
   }, [currentUser]);
 
   const addNote = async (newNote: Omit<Note, 'noteId'>) => {
+    setLoading(true);
     try {
       await addDoc(notesRef, newNote);
+      setLoading(false);
     } catch (error) {
-      console.error('Error adding note: ', error);
+      setError((error as Error).message || 'An error occurred');
+      setLoading(false);
     }
   };
 
   const deleteNote = async (noteId: string) => {
+    setLoading(true);
     try {
       await deleteDoc(doc(notesRef, noteId));
       console.log('Note deleted successfully');
+      setLoading(false);
     } catch (error) {
-      console.error('Error deleting note: ', error);
+      setError((error as Error).message || 'An error occurred');
+      setLoading(false);
     }
   };
 
-  return { notes, setNotes, addNote, deleteNote };
+  return { notes, loading, error, addNote, deleteNote };
 };
 
 export default useFirebase;
