8ceb0f27b9cd448d29ac792fe6106cbf05c08802
urosrelic <urosrelicc@gmail.com>
2024-04-02 12:21:24 +0200

    feat: move fetch login inside a hook

--- src/components/Notes/Notes.tsx
+++ src/components/Notes/Notes.tsx
@@ -1,53 +1,18 @@
 import { useMediaQuery } from '@uidotdev/usehooks';
-import firebase from 'firebase/compat/app';
-import { collection, onSnapshot, query, where } from 'firebase/firestore';
-import { useEffect, useMemo, useState } from 'react';
-import { db } from '../../config/firebase';
 import { useAuth } from '../../hooks/useAuth';
+import useFetch from '../../hooks/useFetch';
 import { GridProps } from '../../utils/types/GridProps';
 import Note from './Note';
 import './Notes.css';
 
-interface Note {
-  noteId: string;
-  archived: boolean;
-  color: string;
-  content: string;
-  createdAt: firebase.firestore.Timestamp;
-  labels: string[];
-  pinned: boolean;
-  title: string;
-  userId: string;
-}
-
 const Notes = ({ gridView }: GridProps) => {
-  const [notes, setNotes] = useState<Note[] | null>(null);
-
   const notesClassName = gridView ? 'grid-view' : 'list-view';
 
   const largerScreen = useMediaQuery('only screen and (min-width: 452px)');
 
-  const notesRef = useMemo(() => collection(db, 'notes'), []);
-
   const { currentUser } = useAuth();
 
-  useEffect(() => {
-    if (currentUser) {
-      const q = query(notesRef, where('userId', '==', currentUser.uid));
-      const unsub = onSnapshot(q, (querySnapshot) => {
-        const items: Note[] = [];
-        querySnapshot.forEach((doc) => {
-          const data = doc.data() as Note;
-          items.push({ noteId: doc.id, ...data });
-        });
-        setNotes(items);
-        console.log(items);
-      });
-      return () => {
-        unsub();
-      };
-    }
-  }, [currentUser]);
+  const { notes } = useFetch(currentUser);
 
   const smallScreenLayout = () => {
     return (
