c7114a2340fa52696488557659c08b2b440b696c
urosrelic <urosrelicc@gmail.com>
2024-04-06 00:20:35 +0200

    feat: add selected note context

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { ColorPickerProvider } from './context/ColorPickerContext.tsx';
+import { SelectedLabelProvider } from './context/SelectedLabelContext.tsx';
 import { SelectedNoteProvider } from './context/SelectedNoteContext.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <SelectedNoteProvider>
-      <ColorPickerProvider>
-        <App />
-      </ColorPickerProvider>
+      <SelectedLabelProvider>
+        <ColorPickerProvider>
+          <App />
+        </ColorPickerProvider>
+      </SelectedLabelProvider>
     </SelectedNoteProvider>
   </React.StrictMode>
 );
